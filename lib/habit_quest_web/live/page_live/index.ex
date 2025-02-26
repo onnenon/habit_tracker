494cd38660d79fea7c68032ed422c4b940dc1b76
Stephen Onnen <stephen.onnen@gmail.com>
2025-02-25 23:59:40 -0600

    updates

--- lib/habit_quest_web/live/page_live/index.ex
+++ lib/habit_quest_web/live/page_live/index.ex
@@ -2,9 +2,16 @@ defmodule HabitQuestWeb.PageLive.Index do
   use HabitQuestWeb, :live_view
 
   alias HabitQuest.Children
+  alias HabitQuest.Children.Child
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, stream(socket, :children, Children.list_children())}
+    children = Children.list_children()
+    |> Enum.map(fn child ->
+      Child.changeset(child, %{})
+      |> Ecto.Changeset.apply_changes()
+    end)
+
+    {:ok, stream(socket, :children, children)}
   end
 end
