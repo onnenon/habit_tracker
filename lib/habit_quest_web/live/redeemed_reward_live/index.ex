729101ee2a11ccea1f188e3b8b9c69c7b3308de4
Stephen Onnen <stephen.onnen@gmail.com>
2025-02-27 17:59:56 -0600

    Update redeemed

--- lib/habit_quest_web/live/redeemed_reward_live/index.ex
+++ lib/habit_quest_web/live/redeemed_reward_live/index.ex
@@ -13,27 +13,36 @@ defmodule HabitQuestWeb.RedeemedRewardLive.Index do
   @impl true
   def handle_event("toggle_fulfilled", _params, socket) do
     show_fulfilled = !socket.assigns.show_fulfilled
+
     {:noreply,
      socket
      |> assign(:show_fulfilled, show_fulfilled)
-     |> assign(:redeemed_rewards, list_redeemed_rewards(if show_fulfilled, do: nil, else: false))}
+     |> assign(:redeemed_rewards, list_redeemed_rewards(show_fulfilled))}
   end
 
   @impl true
   def handle_event("mark_fulfilled", %{"id" => id}, socket) do
     redeemed_reward = Rewards.get_redeemed_reward!(id)
-    {:ok, _updated_reward} = Rewards.update_redeemed_reward(redeemed_reward, %{
-      fulfilled: true,
-      fulfilled_at: DateTime.utc_now()
-    })
+
+    {:ok, _updated_reward} =
+      Rewards.update_redeemed_reward(redeemed_reward, %{
+        fulfilled: true,
+        fulfilled_at: DateTime.utc_now()
+      })
 
     {:noreply,
      socket
      |> put_flash(:info, "Reward marked as fulfilled")
-     |> assign(:redeemed_rewards, list_redeemed_rewards(if socket.assigns.show_fulfilled, do: nil, else: false))}
+     |> assign(:redeemed_rewards, list_redeemed_rewards(socket.assigns.show_fulfilled))}
   end
 
-  defp list_redeemed_rewards(fulfilled) do
-    Rewards.list_redeemed_rewards(fulfilled)
+  defp list_redeemed_rewards(show_fulfilled) do
+    # When show_fulfilled is false, only get unfulfilled rewards
+    # When true, get all rewards and sort them with fulfilled at the end
+    if(show_fulfilled,
+      do: Rewards.list_redeemed_rewards(nil),
+      else: Rewards.list_redeemed_rewards(false)
+    )
+    |> Enum.sort_by(&{&1.fulfilled, &1.redeemed_at}, :asc)
   end
 end
