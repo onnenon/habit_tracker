70f214d3a65d138aff6e37a807ec7e9350c35d3e
Stephen Onnen <stephen.onnen@gmail.com>
2025-03-02 12:04:25 -0600

    add extras

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule HabitQuest.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:ecto_sqlite3_extras, "~> 1.2.2"}
     ]
   end
 
